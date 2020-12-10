9d4ec43b7e5330dd712b2aa5b69ee3585eb448f4
hewayman <hewayman@gmail.com>
2020-12-10 14:22:39 -0500

    navbar changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Auth from './components/Auth/Auth';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Navbar from './components/site/Navbar'
 import Register from './components/site/Register'
 import Login from './components/site/Login'
@@ -12,6 +12,9 @@ class App extends React.Component {
       <div> 
         <Router>
           <Navbar/>
+          <Switch>
+
+          </Switch>
           {/* <Register/>
           <Login /> */}
         </Router>
--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -1,22 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Route } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import CloseIcon from '@material-ui/icons/Close';
+import Login from './Login';
+import { type } from 'os';
 
-class Navbar extends React.Component {
+// type NavbarProps = {
+//   siteName?: string;
+//   username?: string;
+// }
+
+type NavbarState = {
+  sidebar: boolean;
+}
+
+export default class Navbar extends React.Component<{}, NavbarState> {
+  constructor (props: {}) {
+    super(props);
+    this.state = {
+      sidebar: false
+    }
+  }
+  
+  // const [sidebar, setSidebar] = useState({});
+
+  showSidebar = () => () => {
+    const [sidebar, setSidebar] = useState({})
+    setSidebar(!sidebar)
+
+    return setSidebar;
+  } 
+  
   render () {
     return (
-      <div>
-        <AppBar position="static">
+      <>
+        <div>
+          <Link to="#" className='menu-account'>
+            <AccountCircleIcon />
+          </Link>
+        </div>
+        {/* <nav className={this.showSidebar ? 'nav-menu active' : 'nav-menu'}> */}
+        <nav className={this. ? console.log('active') : console.log('not active')}>
+          <ul className='nav-menu-items' onClick={this.showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-account'>
+                <CloseIcon />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" className="storeName">
-              Store Name
-            </Typography>
-            {/* <Button color="inherit">Cart</Button> */}
+              <Link to="/">Store Name</Link>
+            </Typography> */}
+            {/* <Button color="inherit">Cart</Button>
             <IconButton edge="start" className="cartIconButton" color="inherit" aria-label="menu">
               <ShoppingCartIcon />
             </IconButton>
@@ -24,14 +68,15 @@ class Navbar extends React.Component {
               Welcome, User
             </Typography>
             <IconButton edge="start" className="accountIconButton" color="inherit" aria-label="menu">
-              {/* <Link to="/Login"><AccountCircleIcon /></Link> */}
-              <AccountCircleIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-      </div>
+              <Route path="/Login" component={Login}><AccountCircleIcon /></Route> */}
+              {/* <AccountCircleIcon /> */}
+            {/* </IconButton> */}
+          {/* </Toolbar>
+        </AppBar> */}
+        
+      </>
     )
   }
 }
 
-export default Navbar;
+// export default Navbar;
