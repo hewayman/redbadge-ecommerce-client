7b75f995c4a5dc856981564d206d1ebb8cea345e
hewayman <hewayman@gmail.com>
2020-12-09 23:56:45 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Auth from './components/Auth/Auth';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/site/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Router>
+
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/site/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+class Navbar extends React.Component {
+  render () {
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className="storeName">
+              Store Name
+            </Typography>
+            {/* <Button color="inherit">Cart</Button> */}
+            <IconButton edge="start" className="cartIconButton" color="inherit" aria-label="menu">
+              <ShoppingCartIcon />
+            </IconButton>
+            <Typography variant="p" className="welcomeText">
+              Welcome, User
+            </Typography>
+            <IconButton edge="start" className="accountIconButton" color="inherit" aria-label="menu">
+              <AccountCircleIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
